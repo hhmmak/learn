4c22c24405ee7f06bd016776bee769b4a9d88b0d
Hermanda Mak <hermandamak@gmail.com>
2023-01-20 14:05:35 -0800

    basic tests

--- react_jest/test_one/src/component/Todo.test.js
+++ react_jest/test_one/src/component/Todo.test.js
@@ -1,3 +1,33 @@
+import {render, screen, cleanup} from '@testing-library/react';
+import Todo from './Todo';
+
+afterEach(() => {
+  cleanup();
+});
+
 test('toBeTrue', () => {
   expect(true).toBe(true);
+})
+
+test('should render non-completed todo', () => {
+
+  const item = {id: 1, title: 'wash dishes', completed: false};
+
+  render(<Todo item={item}/>);
+  const todoElement = screen.getByTestId('todo-1');
+  expect(todoElement).toBeInTheDocument();
+  expect(todoElement).toHaveTextContent('wash dishes');
+  expect(todoElement).toContainHTML('div');
+  expect(todoElement).not.toContainHTML('strike');
+})
+
+test('should render completed todo', () => {
+
+  const item = {id: 2, title: 'do laundry', completed: true};
+
+  render(<Todo item={item}/>);
+  const todoElement = screen.getByTestId('todo-2');
+  expect(todoElement).toBeInTheDocument();
+  expect(todoElement).toHaveTextContent('do laundry');
+  expect(todoElement).toContainHTML('strike');
 })

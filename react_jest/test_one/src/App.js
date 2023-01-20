d2d94872daf39b386fab2e67895f69218d9a5128
Hermanda Mak <hermandamak@gmail.com>
2023-01-20 14:05:13 -0800

    add props

--- react_jest/test_one/src/App.js
+++ react_jest/test_one/src/App.js
@@ -2,9 +2,18 @@ import Todo from './component/Todo';
 import './App.css';
 
 function App() {
+
+  
+  const todos =[
+    {id: 1, title: 'wash dishes', completed: false},
+    {id: 2, title: 'do laundry', completed: true},
+  ];
+
   return (
     <div className="App">
-      <Todo />
+      { todos.map((item) => 
+        <Todo item={item} />
+      )}
     </div>
   );
 }
--- react_jest/test_one/src/component/Todo.js
+++ react_jest/test_one/src/component/Todo.js
@@ -1,6 +1,12 @@
-const Todo = () => {
+const Todo = ({item}) => {
+
+  const {id, title, completed} = item;
+  
+  const h1 = <h1>{title}</h1>;
+  const text = completed? <strike>{h1}</strike> : h1;
+
   return (
-    <div>Hello World!</div>
+    <div data-testid={`todo-${id}`}>{text}</div>
   )
 }
 export default Todo;

529a42d5350cddac19c39142f4e60e15c4099eb4
Hermanda Mak <hermandamak@gmail.com>
2023-01-31 15:41:34 -0800

    add initial instances and methods

--- sameple_python_OOP/modules/user.py
+++ sameple_python_OOP/modules/user.py
@@ -1,2 +1,18 @@
 class User:
-    pass
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 0
+
+    def make_deposit(self, amount):
+        self.account_balance += amount
+    
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: ${self.account_balance}")
+
+    def transfer_money(self, other_user, amount):
+        self.account_balance -= amount
+        other_user.account_balance += amount

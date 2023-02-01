3b7bca77dd3a958e5d61426127ead24a96dd8a4b
Hermanda Mak <hermandamak@gmail.com>
2023-01-31 18:22:08 -0800

    add retirementAccount module

--- sameple_python_OOP/modules/user.py
+++ sameple_python_OOP/modules/user.py
@@ -1,38 +1,45 @@
 from bankAccount import BankAccount
+from retirementAccount import RetirementAccount
 
 class User:
 
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.account = [BankAccount()]
+        self.account = {"regular" : [BankAccount()]}
 
-    def create_account(self):
-        self.account.append(BankAccount())
+    def create_account(self, account_type="regular"):
+        if account_type not in self.account:
+            self.account[account_type] = []
+
+        if account_type == "regular":
+            self.account[account_type].append(BankAccount())
+        elif account_type == "retirement":
+            self.account[account_type].append(RetirementAccount())
         return self
 
-    def make_deposit(self, amount, num=0):
-        self.account[num].deposit(amount)
+    def make_deposit(self, amount, num=0, account_type="regular"):
+        self.account[account_type][num].deposit(amount)
         return self
     
-    def make_withdrawal(self, amount, num=0):
-        self.account[num].withdraw(amount)
+    def make_withdrawal(self, amount, num=0, account_type="regular"):
+        self.account[account_type][num].withdraw(amount)
         return self
 
-    def display_user_balance(self, num=0):
-        print(f"User: {self.name}, Balance of account #{num}: ${self.account[num].balance}")
-        if len(self.account) > 1:
-            print(f"Total Balance: ${User.total_sum(self.account)}")    #implement staticmethod, print only if user have more than one account
+    def display_user_balance(self, num=0, account_type="regular"):
+        print(f"User: {self.name}, Balance of account #{num}: ${self.account[account_type][num].balance}")
+        print(f"Total Balance: ${User.total_sum(self.account)}")
         return self
 
     def transfer_money(self, other_user, amount, num_self=0, num_other_user=0):
-        self.account[num_self].withdraw(amount)
-        other_user.account[num_other_user].deposit(amount)
+        self.account["regular"][num_self].withdraw(amount)
+        other_user.account["regular"][num_other_user].deposit(amount)
         return self
 
     @staticmethod   
     def total_sum(account_list):
         sum_num = 0
-        for account in account_list:
-            sum_num += account.balance
+        for account_type in account_list:
+            for account in account_list[account_type]:
+                sum_num += account.balance
         return sum_num

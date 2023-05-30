1aa4fec0c08691596bca4293cc6eb6aaa7ae70d0
Hermanda Mak <hermandamak@gmail.com>
2023-05-30 14:04:56 -0700

    add private/public methods

--- typescript/classes/src/index.ts
+++ typescript/classes/src/index.ts
@@ -20,12 +20,32 @@ class User {
   
   city: string = "";
   readonly adult: boolean = this.age >= 19;
-  
+  private _friendsCount: number = 0;
   // define type and public/private for properties same time as parameter, shorthand
   constructor(
     public email: string, 
     public name: string, 
     private age: number){
+  }
+
+  //define public/private methods
+  consoleLogUserAdult(){
+    console.log(`User: ${this.email}, ${this.name}, ${this.city}, adult: ${this.adult}`)
+  }
+  private consoleLogUserAge(){
+    console.log(`User: ${this.email}, ${this.name}, ${this.city}, age: ${this.age}`)
+  }
+
+  //getters and setters for private methods
+  get friendsCount(): number{
+    return this._friendsCount;
+  }
+  // no return type statement required for setters
+  set friendsCount(changeNum){
+    if (changeNum > this._friendsCount){
+      throw new Error("Cannot have less than 0 friends")
+    }
+    this._friendsCount += changeNum;
   }
 }
 

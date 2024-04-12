59dc19f77002ce94fe3b1c76a4de8fdd7b195346
Hermanda Mak <hermandamak@gmail.com>
2024-04-12 16:57:36 -0700

    add create_order and capture_order

--- paypal/server/flask_app/routes.py
+++ paypal/server/flask_app/routes.py
@@ -1,6 +1,5 @@
-from flask import jsonify, request
 from app import app
-import os, base64, requests
+import os, requests, json
 
 # Home route
 @app.route('/')
@@ -10,8 +9,6 @@ def hello_world():
 @app.route('/test')
 def hello_world_test():
     return 'Hello, World! Test'
-
-
 
 # get access token from paypal
 @app.route('/access-token')
@@ -31,5 +28,58 @@ def get_access_token():
 
     response = requests.post(
         f'{os.environ.get("PAYPAL_BASE")}/v1/oauth2/token', auth=(client_id,client_secret), headers=headers, data=data)
+
+    return response.json()
+
+@app.route('/create-order')
+def create_order():
+
+    cart = {
+                "reference_id": "cba1",
+                "amount": {
+                    "currency_code": "USD",
+                    "value": "100.00"
+                }
+            }
+    token = get_access_token()['access_token']
+
+    url = f'{os.environ.get("PAYPAL_BASE")}/v2/checkout/orders'
+    payload = {
+        "intent": "CAPTURE",
+        "purchase_units": [cart],
+    }
+
+    headers = {
+        'Content-Type': 'application/json',
+        'PayPal-Request-Id': 'abc1',
+        'Authorization': f'Bearer {token}',
+        # Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+        # https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+        # "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
+        # "PayPal-Mock-Response": '{"mock_application_codes": "PERMISSION_DENIED"}'
+        # "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+    return response.json()
+
+def capture_order(order_id):
+
+    token = get_access_token()['access_token']
+
+    url = f'{os.environ.get("PAYPAL_BASE")}/v2/checkout/orders/{order_id}/capture'
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}',
+        # Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+        # https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+        # "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
+        # "PayPal-Mock-Response": '{"mock_application_codes": "PERMISSION_DENIED"}'
+        # "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    }
+
+    response = requests.post(url, headers=headers)
 
     return response.json()

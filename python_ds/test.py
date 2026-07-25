2a9c519150f92bf4ba1d05111136c43b0eeb98ad
Hermanda Mak <hermandamak@gmail.com>
2026-07-24 18:23:21 -0700

    ipynb basic

---
+++ python_ds/test.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+np.random.seed(904052499)
+print(np.random.rand())
+print(np.random.rand())
+print(np.random.rand())
+np.random.seed(904052499)
+print(np.random.rand())
+print(np.random.rand())
+print(np.random.rand())
